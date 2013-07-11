c52e9a8377db4b8fa11716e9ce5a3ad5e0c21b9c
Mark Soper <masoper@gmail.com>
2013-07-11 10:26:38 -0400

    sleep

--- 12July2013/sleep.md
+++ 12July2013/sleep.md
@@ -2,5 +2,9 @@
 ## Go The F*ck To Sleep Or Else Be Dumb
 
 Kids who had irregular bedtimes on school nights at ages 3,5, and 7 underperformed in reading, math, and spatial ability tests taken at age 7, according to a [study by University College London](
-http://www.bloomberg.com/news/2013-07-08/irregular-bedtimes-tied-to-children-s-lower-test-scores.html "Irregular Bedtimes Tied to Children’s Lower Test Scores")
+http://www.bloomberg.com/news/2013-07-08/irregular-bedtimes-tied-to-children-s-lower-test-scores.html "Irregular Bedtimes Tied to Children’s Lower Test Scores") that was widely reported this week. The study shows correlation but not causation, perhaps not adding much to the already established facts about the importance of sleep in kids' mental and emotional functioning. Po Bronson's New York Magazine "Snooze Or Loose" for a great overview of children's sleep research.
+
+
+
+
 
--- edition.py
+++ edition.py
@@ -2,6 +2,7 @@
 import sys
 import markdown
 import json
+import codecs
 
 dirname = sys.argv[1].rstrip("/")
 
@@ -11,7 +12,9 @@ articleNamesFile.close()
 
 articlesHtml = []
 for article in articles:
-    mdfile = open(dirname + "/" + article, "r")
+    #mdfile = open(dirname + "/" + article, "r")
+    mdfile = codecs.open(dirname + "/" + article, mode="r", encoding="utf-8")
+    #md = unicode(mdfile.read(), errors="replace")
     md = mdfile.read()
     mdfile.close()
     html = markdown.markdown(md)
@@ -20,6 +23,14 @@ for article in articles:
 delim = "\n\n\n"
 html = delim + delim.join(articlesHtml) + delim
 
-htmlFile = open(dirname + "/" + dirname + ".html", "w")
+#htmlFile = open(dirname + "/" + dirname + ".html", "w")
+
+htmlFile = codecs.open(dirname + "/" + dirname + ".html",
+	"w",
+	encoding="utf-8", 
+    errors="xmlcharrefreplace"
+)
+
 htmlFile.write(html)
 htmlFile.close()
+
