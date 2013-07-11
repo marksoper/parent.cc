4c160612739ffde09c227ec0a0b1ac04f74ca52d
Mark Soper <masoper@gmail.com>
2013-07-11 09:19:52 -0400

    new edition.py

--- edition.py
+++ edition.py
@@ -1,15 +1,17 @@
 
-import codecs
+import sys
 import markdown
 import json
 
-articleNamesFile = open("./28June2013.json", "r")
+dirname = sys.argv[1].rstrip("/")
+
+articleNamesFile = open(dirname + "/edition.json", "r")
 articles = json.loads(articleNamesFile.read())
 articleNamesFile.close()
 
 articlesHtml = []
 for article in articles:
-    mdfile = open("./" + article, "r")
+    mdfile = open(dirname + "/" + article, "r")
     md = mdfile.read()
     mdfile.close()
     html = markdown.markdown(md)
@@ -18,6 +20,6 @@ for article in articles:
 delim = "\n\n\n"
 html = delim + delim.join(articlesHtml) + delim
 
-htmlFile = open("./28June2013.html", "w")
+htmlFile = open(dirname + "/" + dirname + ".html", "w")
 htmlFile.write(html)
 htmlFile.close()
